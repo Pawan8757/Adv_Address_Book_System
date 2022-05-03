c365ffb5229ffd7aeb34c1e7100b3dd99eefe309
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-03 17:57:45 +0530

    program for adding new contact detail

---
+++ UC-2/Address_Book_System_Problem/AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System_Problem
+{
+    internal class AddressBook
+    {
+        private List<Person> AddedPerson = new List<Person>();
+
+        public void AddRecords(Person p)
+        {
+            AddedPerson.Add(p);
+            Console.WriteLine("\nRecord successfully Added in Address Book");
+        }
+
+        public void DisplayRecords()
+        {
+            Console.WriteLine("\nThe Contact Details Available in Address Book are:\n");
+            foreach (Person p in AddedPerson)
+                Console.WriteLine(p.DisplaySavedRecords());
+        }
+    }
+}
---
+++ UC-2/Address_Book_System_Problem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Address_Book_System_Problem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the tutorial of address book includation part");
+
+           AddressBook address= new AddressBook();
+
+            Person person= new Person();
+            person.AddingPerson();
+            address.AddRecords(person);
+            address.DisplayRecords();
+
+        }
+    }
+}
