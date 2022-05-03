6ae991f2f9acb5e07a1428933d944e07144ac5a4
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-03 17:56:40 +0530

    program for creating contact detail

---
+++ UC-1/Address_Book_System_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Address_Book_System_Problem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the tutorial of address book includation part");
+
+            Person person = new Person();
+            person.AddingPerson();
+            Console.WriteLine("\n------------Here is the detailed form of person----------- ");
+
+            Console.WriteLine("Person's Detail=" + person.DisplaySavedRecords());
+
+        }
+    }
+}
