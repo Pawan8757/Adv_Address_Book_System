a4dd3b885bd292bc652af6f6a3c76b90a624254b
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-03 17:59:04 +0530

    program for editing existing  contact detail

---
+++ UC-3/Address_Book_System_Problem/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System_Problem
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int ZipCode;
+        public long PhoneNumber;
+        public string EmailId;
+
+        public void AddingPerson()
+        {
+            Console.WriteLine("Enter First-Name: ");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last-Name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter City: ");
+            City = Console.ReadLine();
+            Console.WriteLine("Enter State: ");
+            State = Console.ReadLine();
+            Console.WriteLine("Enter Zip-Code: ");
+            ZipCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ContactNumber: ");
+            PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email-Id: ");
+            EmailId = Console.ReadLine();
+        }
+        public  string DisplaySavedRecords()
+        {
+            return "\nFirst Name: " + FirstName + "\nLast Name:  " + LastName + "\nAddress: " + Address + "\nCity: " + City
+                + "\nState: " + State + "\nZip-Code: " + ZipCode + "\nPhone: " + PhoneNumber + "\nEmail-Id: " + EmailId ;
+        }
+    }
+    }
---
+++ UC-3/Address_Book_System_Problem/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Address_Book_System_Problem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the tutorial of address book includation part");
+            
+
+            AddressBook address = new AddressBook();
+            Person person = new();
+
+            string input;
+            do
+            {
+                UserMenu();
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "A":
+                        Console.WriteLine("\nEnter Details to Add New Contact-");
+                        person.AddingPerson();
+                        address.AddRecords(person);
+                        break;
+                    case "E":
+                        Console.WriteLine("\nEnter Name of the person to edit-");
+                        Console.Write("First Name: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Last Name: ");
+                        string lastName = Console.ReadLine();
+                        address.EditRecords(firstName, lastName);
+                        break;
+                    case "P":
+                        address.DisplayRecords();
+                        break;
+                    case "X":
+                        Console.WriteLine("\nProgram Exited Successfully.");
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid Input.");
+                        break;
+                }
+            }
+            while (input != "X");
+
+        }
+
+        public static void UserMenu()
+        {
+            Console.WriteLine("\n--------User Menu--------");
+            Console.WriteLine("Enter 'A' to add new contact.");
+            Console.WriteLine("Enter 'E' to edit existing contact.");
+            Console.WriteLine("Enter 'P' to display all address book contacts");
+            Console.WriteLine("Enter 'X' to exit.");
+            Console.Write("Input: ");
+        }
+    }
+}
